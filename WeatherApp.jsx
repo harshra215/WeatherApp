a1bc8d3e3767e69ff5f9ad47266810716cd175e6
Harsh ranjan <67137451+harshra215@users.noreply.github.com>
2024-01-18 14:27:25 +0530

    Add files via upload

--- WeatherApp.jsx
+++ WeatherApp.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import './WeatherApp.css'
+import React, {useState} from "react"; 
+import "./WeatherApp.css";
 
 import search_icon from '../assets/search.png';
-import clear_icon from '../assets/Clear.png';
-import cloud_icon from '../assets/Cloud.png';
+import clear_icon from '../assets/clear.png';
+import cloud_icon from '../assets/cloud.png';
 import drizzle_icon from '../assets/drizzle.png';
 import rain_icon from '../assets/rain.png';
 import snow_icon from '../assets/snow.png';
-import wind_icon from '../assets/windy.png';
+import wind_icon from '../assets/wind.png';
 import humidity_icon from '../assets/humidity.png';
 
-const WeatherApp = async () => {
+const WeatherApp =  () => {
 
   let api_key ="8f0bbb0b36bcfb85db0048ae1c9f8608";
 
-  const search = () => {
-    const element =document.getElementsByClassName("");
+  const [wicon,setWicon] = useState(cloud_icon);
+
+  const search = async () => {
+    const element =document.getElementsByClassName("cityInput");
     if(element[0].value==="")
     {
       return 0;
@@ -23,17 +25,48 @@ const WeatherApp = async () => {
     let url =`https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
 
     let response = await fetch(url);
-    let data = await response.json();
+    let data =  await response.json();
     const humidity =document.getElementsByClassName("humidity-percent");
     const wind=document.getElementsByClassName("wind-rate");
     const temperature = document.getElementsByClassName("weather-temp");
     const location = document.getElementsByClassName("weather-location");
 
     humidity[0].innerHTML = data.main.humidity+ " %";
-    wind[0].innerHTML = data.wind.speed+ " km/h";
-    temperature[0].innerHTML = data.main.temp+"℃";
+    wind[0].innerHTML = Math.floor(data.wind.speed)+ " km/h";
+    temperature[0].innerHTML = Math.floor(data.main.temp)+"℃";
     location[0].innerHTML = data.name;
 
+    if( data.weather[0].icon==="01d" || data.weather[0].icon==="01n")
+    {
+      setWicon(clear_icon);
+    }
+    else if(data.weather[0].icon==="02d"  || data.weather[0].icon==="02n")
+    {
+      setWicon(cloud_icon);
+    }
+    else if(data.weather[0].icon==="03d"  || data.weather[0].icon==="03n")
+    {
+      setWicon(drizzle_icon);
+    }
+    else if(data.weather[0].icon==="04d"  || data.weather[0].icon==="04n")
+    {
+      setWicon(drizzle_icon);
+    }
+    else if(data.weather[0].icon==="09d"  || data.weather[0].icon==="09n")
+    {
+      setWicon(rain_icon);
+    }
+    else if(data.weather[0].icon==="10d"  || data.weather[0].icon==="10n")
+    {
+      setWicon(rain_icon);
+    }
+    else if(data.weather[0].icon==="13d"  || data.weather[0].icon==="13n")
+    {
+      setWicon(snow_icon);
+    }
+    else{
+      setWicon(clear_icon);
+    }
 
   }
 
